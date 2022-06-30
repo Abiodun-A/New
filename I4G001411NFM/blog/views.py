0ec317b22e031293e4334c5d9bedf536b9e8d7bd
Emmanuel Abiodun Akinjogunla <105178231+Abiodun-A@users.noreply.github.com>
2022-06-30 17:42:14 +0100

    Update views.py

--- I4G001411NFM/blog/views.py
+++ I4G001411NFM/blog/views.py
@@ -11,11 +11,12 @@ from .models import Post
 # Create your views here.
 class PostListView(ListView):
     model = Post
-    template_name = 'blog.html'
+    
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url: reverse_lazy("blog:all")
+    template_name = 'blog.html'
 
 class PostDetailView(DetailView):
     model = Post
