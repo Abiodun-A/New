8f26841cbf3399e374d0c8038bebd3ee1a7c4b65
Emmanuel Abiodun Akinjogunla <105178231+Abiodun-A@users.noreply.github.com>
2022-06-30 16:50:17 +0100

    Update views.py

--- I4G001411NFM/blog/views.py
+++ I4G001411NFM/blog/views.py
@@ -1,13 +1,17 @@
 from pyexpat import model
+from tempfile import template
+from unicodedata import name
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import ListView, CreateView, DetailView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Post
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
-
+    template_name = 'blog.html'
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
